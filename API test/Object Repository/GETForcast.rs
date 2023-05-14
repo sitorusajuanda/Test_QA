2f85fd160a8af9b8cf667851c2bcb187ba135f18
Ajuanda Sitorus <sitorus.ajuanda@gmail.com>
2023-05-15 02:48:49 +0700

    test

---
+++ API test/Object Repository/GETForcast.rs
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>GETForcast</name>
+   <tag></tag>
+   <elementGuidId>1f0c4ab2-3a8e-44b5-bcae-7c884708e441</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <katalonVersion>8.5.2</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>http://api.weatherbit.io/v2.0//current?postal_code=${postal_code}&amp;key=${key}</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>'40.730610 '</defaultValue>
+      <description></description>
+      <id>26e66eda-670d-43db-a5c4-ecb076da2ba4</id>
+      <masked>false</masked>
+      <name>lat</name>
+   </variables>
+   <variables>
+      <defaultValue>'-73.935242 '</defaultValue>
+      <description></description>
+      <id>f454d503-d79f-4fd7-8272-c9f38b785fa6</id>
+      <masked>false</masked>
+      <name>lon</name>
+   </variables>
+   <variables>
+      <defaultValue>'495c51f57c9345e080b3c72ae3c0ce02'</defaultValue>
+      <description></description>
+      <id>978fc520-3694-42ff-bac3-008fb1afc50e</id>
+      <masked>false</masked>
+      <name>key</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ API test/Object Repository/GetCurrent.rs
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>GetCurrent</name>
+   <tag></tag>
+   <elementGuidId>0b8d3117-f0b7-4316-bae6-0b72cda946e3</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <katalonVersion>8.5.2</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://api.weatherbit.io/v2.0/current?lat=${lat} &amp;lon=${lon} &amp;key=${key}</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>09116420-892c-4280-a791-de386dc975b1</id>
+      <masked>false</masked>
+      <name>variable</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>00686fd2-12be-48d9-9522-9ddcadecbcb8</id>
+      <masked>false</masked>
+      <name>variable_0</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>699463b5-2ccf-4f53-88cb-e57e3d2dffe0</id>
+      <masked>false</masked>
+      <name>variable_1</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>695076f2-af34-4939-9a28-809b2c152de5</id>
+      <masked>false</masked>
+      <name>variable_2</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>548168a9-c447-48fe-b830-49abb24a7e29</id>
+      <masked>false</masked>
+      <name>variable_3</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>fbb81582-3c49-4781-892f-711f1cf80c20</id>
+      <masked>false</masked>
+      <name>variable_4</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+WS.verifyElementPropertyValue(response, 'data[0].wind_dir', 9)
+
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
